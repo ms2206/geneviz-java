4fcd5bf953f191094658cef778acb6f33ffecb92
Matthew Spriggs <matspriggs@gmail.com>
2025-11-16 12:29:50 +0000

    Add FastaParser class with method for parsing FASTA files

---
+++ geneviz-java/src/main/java/GVJ/io/FastaParser.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GVJ.io;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+import org.biojava.nbio.core.sequence.DNASequence;
+
+/**
+ *
+ * @author mspriggs
+ */
+public class FastaParser {
+    public static Map<String, DNASequence> parseFasta(Path file) throws IOException {
+        // Implement FASTA parsing logic here
+        // Placeholder implementation
+        return null;
+    }
+
+}
