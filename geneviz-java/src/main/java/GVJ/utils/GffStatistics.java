f9c29f77f898497a1f9a5e425067ad8c906d45c3
Matthew Spriggs <matspriggs@gmail.com>
2025-11-18 09:55:57 +0000

    Add GffStatistics class for GFF file analysis methods

---
+++ geneviz-java/src/main/java/GVJ/utils/GffStatistics.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GVJ.utils;
+
+import org.biojava.nbio.genome.parsers.gff.FeatureList;
+
+/**
+ *
+ * @author mspriggs
+ */
+public class GffStatistics {
+
+    /**
+     * Calculate the average number of exons per gene
+     * 
+     * @param features The FeatureList from a parsed GFF file
+     * @return The average number of exons per gene
+     */
+    public static double getAverageExonsPerGene(FeatureList features) {
+        // Logic here
+        return 0;
+    }
+
+    /**
+     * Find the longest and shortest features in the FeatureList
+     * 
+     * @param features
+     * @return
+     */
+    public static FeatureList longestShortestFeatures(FeatureList features) {
+        // Logic here
+        return null;
+    }
+
+    /**
+     * Calculate the average gene length from the FeatureList
+     * 
+     * @param features The FeatureList from a parsed GFF file
+     * @return The average gene length
+     */
+    public static double averageGeneLength(FeatureList features) {
+        // Logic here
+        return 0;
+    }
+
+}
