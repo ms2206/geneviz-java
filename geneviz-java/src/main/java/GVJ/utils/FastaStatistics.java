da98db539b83e780e012afc71400447f69b4f8c8
Matthew Spriggs <matspriggs@gmail.com>
2025-11-18 09:55:49 +0000

    Add FastaStatistics class with methods for sequence analysis

---
+++ geneviz-java/src/main/java/GVJ/utils/FastaStatistics.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GVJ.utils;
+
+/**
+ *
+ * @author mspriggs
+ */
+public class FastaStatistics {
+
+    /*
+     * get length of sequence
+     */
+    public static int getLength(String features) {
+        // Logic here
+        return 0;
+    }
+
+    /*
+     * get average length of sequences
+     */
+    public static double averageLength(String features) {
+        // Logic here
+        return 0;
+    }
+
+    /*
+     * get GC content of sequence
+     */
+    public static double getGCcontent(String features) {
+        // Logic here
+        return 0;
+    }
+
+}
