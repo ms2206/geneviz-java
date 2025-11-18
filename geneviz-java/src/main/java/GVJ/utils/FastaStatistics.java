8f2c87c38feebaf4eda68bbc63851aa0278256ae
Matthew Spriggs <matspriggs@gmail.com>
2025-11-18 13:24:39 +0000

    Add FastaStatistics methods for sequence length and GC content calculations

--- geneviz-java/src/main/java/GVJ/utils/FastaStatistics.java
+++ geneviz-java/src/main/java/GVJ/utils/FastaStatistics.java
@@ -4,34 +4,84 @@
  */
 package GVJ.utils;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import org.biojava.nbio.core.sequence.DNASequence;
+
 /**
  *
  * @author mspriggs
  */
+
 public class FastaStatistics {
 
     /*
-     * get length of sequence
+     * Wrapper to getLength of a FASTA sequence from a DNASequence object.
+     * --- slightly unnecessary but keeps statistics functions together ---
+     * 
+     * @param fastaSequence The DNASequence object representing the FASTA sequence
+     * 
+     * @return The length of the sequence
+     * 
      */
-    public static int getLength(String features) {
-        // Logic here
-        return 0;
+    public static int getSequenceLength(DNASequence fastaSequence) {
+
+        // getLength
+        int length = fastaSequence.getLength();
+
+        return length;
     }
 
     /*
-     * get average length of sequences
+     * Get average length of sequences in a FASTA map
+     * 
+     * @param fastaMap Map of sequence identifiers and DNASequence objects
+     * 
+     * @return The average length of the sequences
+     * 
      */
-    public static double averageLength(String features) {
-        // Logic here
-        return 0;
+    public static double sequencesGetAverageLength(Map<String, DNASequence> fastaMap) {
+
+        int totalLength = 0;
+
+        for (DNASequence seq : fastaMap.values()) {
+            int length = getSequenceLength(seq);
+            totalLength += length;
+        }
+        ;
+        try {
+            double averageLength = (double) totalLength / fastaMap.size();
+            return averageLength;
+        } catch (ArithmeticException e) {
+            return 0;
+        }
+    }
+
+    // Human readable sequencesGetAverageLength overload
+    public static double sequencesGetAverageLength(Map<String, DNASequence> fastaMap, boolean inMbp) {
+        double avgLength = sequencesGetAverageLength(fastaMap);
+        if (inMbp) {
+            return bp2Mbp(avgLength);
+        } else {
+            return avgLength;
+        }
     }
 
     /*
-     * get GC content of sequence
+     * Wrapper to getGCCount() of a FASTA sequence from a DNASequence object.
+     * --- slightly unnecessary but keeps statistics functions together ---
      */
-    public static double getGCcontent(String features) {
-        // Logic here
-        return 0;
+    public static double getSequenceGCcount(DNASequence fastaSequence) {
+
+        // getGCCount
+        int gcCount = fastaSequence.getGCCount();
+
+        return gcCount;
+    }
+
+    private static double bp2Mbp(double basePairs) {
+        return basePairs / 1000000.0;
     }
 
 }
