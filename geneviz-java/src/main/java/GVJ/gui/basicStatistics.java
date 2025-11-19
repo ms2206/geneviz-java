bb6553cb1da218a40678ae4ab722e56e9ab9283b
Matthew Spriggs <matspriggs@gmail.com>
2025-11-19 16:49:21 +0000

    Refactor featureTypes list in basicStatistics to remove 'stop_codon' for improved clarity

--- geneviz-java/src/main/java/GVJ/gui/basicStatistics.java
+++ geneviz-java/src/main/java/GVJ/gui/basicStatistics.java
@@ -388,8 +388,7 @@ public class basicStatistics extends javax.swing.JPanel {
                 // Refresh Feature Selector ComboBox
                 // TODO make this dynamic based on GFF content: Add to tech debt log
 
-                List<String> featureTypes = Arrays.asList("gene", "mRNA", "intron", "exon", "CDS", "start_codon",
-                                "stop_codon");
+                List<String> featureTypes = Arrays.asList("gene", "mRNA", "intron", "exon", "CDS");                         "stop_codon");
                 jComboBoxgetFeature.removeAllItems(); // Clear existing items
                 jComboBoxgetFeature.setEditable(true); // Make it editable for typing search
                 for (String featureType : featureTypes) {
