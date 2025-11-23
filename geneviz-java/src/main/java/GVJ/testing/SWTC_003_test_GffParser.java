5b199ba8736fccdf479903b5fbc323f612b57527
Matthew Spriggs <matspriggs@gmail.com>
2025-11-23 16:51:03 +0000

    Remove unused test classes and commented code from GffParser, GffUtils, and FastaStatistics tests to clean up the codebase. These have hard coded values.

--- geneviz-java/src/main/java/GVJ/testing/SWTC_003_test_GffParser.java
+++ geneviz-java/src/main/java/GVJ/testing/SWTC_003_test_GffParser.java
@@ -1,213 +1,222 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package GVJ.testing;
+// /*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt
+// to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit
+// this template
+// */
+// package GVJ.testing;
 
-import GVJ.io.GffParser;
-import org.biojava.nbio.genome.parsers.gff.FeatureList;
-import org.biojava.nbio.genome.parsers.gff.FeatureI;
-import org.biojava.nbio.genome.parsers.gff.Location;
+// import GVJ.io.GffParser;
+// import org.biojava.nbio.genome.parsers.gff.FeatureList;
+// import org.biojava.nbio.genome.parsers.gff.FeatureI;
+// import org.biojava.nbio.genome.parsers.gff.Location;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.HashMap;
+// import java.io.File;
+// import java.util.Collection;
+// import java.util.HashMap;
 
-/**
- *
- * @author mspriggs
- */
-public class SWTC_003_test_GffParser {
-    public static void main(String[] args) {
-        System.out.println("SWTC_003_test_GffParser");
-        // run Gff3Parser test
-        // System.out.println("Running Gff3Parser_test...");
-        // Gff3Parser_test();
+// /**
+// *
+// * @author mspriggs
+// */
+// public class SWTC_003_test_GffParser {
+// public static void main(String[] args) {
+// System.out.println("SWTC_003_test_GffParser");
+// // run Gff3Parser test
+// // System.out.println("Running Gff3Parser_test...");
+// // Gff3Parser_test();
 
-        // run Gff2Parser test
-        // System.out.println("Running Gff2Parser_test...");
-        // Gff2Parser_test();
+// // run Gff2Parser test
+// // System.out.println("Running Gff2Parser_test...");
+// // Gff2Parser_test();
 
-        // run group by ID test
-        // System.out.println("Running groupById_test...");
-        // groupById();
+// // run group by ID test
+// // System.out.println("Running groupById_test...");
+// // groupById();
 
-        // run group by Attribute test
-        // System.out.println("Running groupByAttribute_test...");
-        // groupByAttribute();
+// // run group by Attribute test
+// // System.out.println("Running groupByAttribute_test...");
+// // groupByAttribute();
 
-        // run select feature by group test
-        // System.out.println("Running selectFeatureByGroup_test...");
-        selectFeatureByGroup();
+// // run select feature by group test
+// // System.out.println("Running selectFeatureByGroup_test...");
+// selectFeatureByGroup();
 
-        // run loop GFF test
-        System.out.println("Running loopGFF_test...");
-        loopGFF();
+// // run loop GFF test
+// System.out.println("Running loopGFF_test...");
+// loopGFF();
 
-    }
+// }
 
-    public static void Gff3Parser_test() {
-        System.out.println("Gff3Parser_test");
+// public static void Gff3Parser_test() {
+// System.out.println("Gff3Parser_test");
 
-        // get filename
-        String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
-        // create File object
-        File file = new File(filepath);
-        // get GffParser for GFF3
-        GffParser parser = GffParser.getParser(3);
-        // parse the file
-        try {
-            FeatureList gff3 = parser.parse(file);
-            // print first 10 characters of gff3 to verify
-            System.out.println(
-                    "Parsed GFF3 FeatureList: " + gff3.toString()
-                            .substring(0, 500));
+// // get filename
+// String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
+// // create File object
+// File file = new File(filepath);
+// // get GffParser for GFF3
+// GffParser parser = GffParser.getParser(3);
+// // parse the file
+// try {
+// FeatureList gff3 = parser.parse(file);
+// // print first 10 characters of gff3 to verify
+// System.out.println(
+// "Parsed GFF3 FeatureList: " + gff3.toString()
+// .substring(0, 500));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+// }
 
-    public static void Gff2Parser_test() {
-        System.out.println("Gff2Parser_test");
+// public static void Gff2Parser_test() {
+// System.out.println("Gff2Parser_test");
 
-        // get filename
-        String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gtf";
-        // create File object
-        File file = new File(filepath);
-        // get GffParser for GFF3
-        GffParser parser = GffParser.getParser(2);
-        // parse the file
-        try {
-            FeatureList gff2 = parser.parse(file);
-            // print first 10 characters of gff3 to verify
-            System.out.println(
-                    "Parsed GFF2 FeatureList: " + gff2.toString()
-                            .substring(0, 500));
+// // get filename
+// String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gtf";
+// // create File object
+// File file = new File(filepath);
+// // get GffParser for GFF3
+// GffParser parser = GffParser.getParser(2);
+// // parse the file
+// try {
+// FeatureList gff2 = parser.parse(file);
+// // print first 10 characters of gff3 to verify
+// System.out.println(
+// "Parsed GFF2 FeatureList: " + gff2.toString()
+// .substring(0, 500));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+// }
 
-    public static void groupById() {
-        // get filename
-        String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
-        // create File object
-        File file = new File(filepath);
-        // get GffParser for GFF3
-        GffParser parser = GffParser.getParser(3);
-        // parse the file
-        try {
-            FeatureList gff3 = parser.parse(file);
+// public static void groupById() {
+// // get filename
+// String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
+// // create File object
+// File file = new File(filepath);
+// // get GffParser for GFF3
+// GffParser parser = GffParser.getParser(3);
+// // parse the file
+// try {
+// FeatureList gff3 = parser.parse(file);
 
-            Collection<String> groupedFeatures = gff3.groupValues();
-            for (String key : groupedFeatures) {
-                System.out.println("Feature key: " + key);
-                break;
-            }
+// Collection<String> groupedFeatures = gff3.groupValues();
+// for (String key : groupedFeatures) {
+// System.out.println("Feature key: " + key);
+// break;
+// }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
 
-    }
+// }
 
-    /*
-     * 
-     * I can use attributeValues to get Parent attributes from the GFF3 file.
-     * This is useful for grouping features by their parent IDs.
-     * Suffexisex like .t1, .start1, .CDS{1++}, .exon{1++}, .intron{1++}, .stop1 are
-     * helpful to identify feature types and their relationships.
-     * 
-     */
-    public static void groupByAttribute() {
-        // get filename
-        String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
-        // create File object
-        File file = new File(filepath);
-        // get GffParser for GFF3
-        GffParser parser = GffParser.getParser(3);
-        // parse the file
-        try {
-            FeatureList gff3 = parser.parse(file);
+// /*
+// *
+// * I can use attributeValues to get Parent attributes from the GFF3 file.
+// * This is useful for grouping features by their parent IDs.
+// * Suffexisex like .t1, .start1, .CDS{1++}, .exon{1++}, .intron{1++}, .stop1
+// are
+// * helpful to identify feature types and their relationships.
+// *
+// */
+// public static void groupByAttribute() {
+// // get filename
+// String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
+// // create File object
+// File file = new File(filepath);
+// // get GffParser for GFF3
+// GffParser parser = GffParser.getParser(3);
+// // parse the file
+// try {
+// FeatureList gff3 = parser.parse(file);
 
-            // Returns: A collection (suitable for iteration using Java's "for" loop) of all
-            // the values found for this key.
-            System.out.println("Attribute Value: " + gff3.attributeValues("Parent"));
+// // Returns: A collection (suitable for iteration using Java's "for" loop) of
+// all
+// // the values found for this key.
+// System.out.println("Attribute Value: " + gff3.attributeValues("Parent"));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
 
-    }
+// }
 
-    /*
-     * 
-     * Seems to be a method to list of all features that have the specified ID.
-     * Useful for retrieving all features associated with a particular feature
-     * e.g. gene001 for gene
-     * gene001.t1 for mRNA
-     * gene001.t1.CDS{1++} for CDS features
-     * gene001.t1.exon{1++} for exon features
-     * gene001.t1.intron{1++} for intron features
-     * gene001.t1.start1 for start codon
-     * gene001.t1.stop1 for stop codon
-     * id.
-     */
-    public static void selectFeatureByGroup() {
-        // get filename
-        String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
-        // create File object
-        File file = new File(filepath);
-        // get GffParser for GFF3
-        GffParser parser = GffParser.getParser(3);
-        // parse the file
-        try {
-            FeatureList gff3 = parser.parse(file);
+// /*
+// *
+// * Seems to be a method to list of all features that have the specified ID.
+// * Useful for retrieving all features associated with a particular feature
+// * e.g. gene001 for gene
+// * gene001.t1 for mRNA
+// * gene001.t1.CDS{1++} for CDS features
+// * gene001.t1.exon{1++} for exon features
+// * gene001.t1.intron{1++} for intron features
+// * gene001.t1.start1 for start codon
+// * gene001.t1.stop1 for stop codon
+// * id.
+// */
+// public static void selectFeatureByGroup() {
+// // get filename
+// String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
+// // create File object
+// File file = new File(filepath);
+// // get GffParser for GFF3
+// GffParser parser = GffParser.getParser(3);
+// // parse the file
+// try {
+// FeatureList gff3 = parser.parse(file);
 
-            FeatureList selectedFeatures = gff3.selectByGroup("ID=jg10174.t1");
-            System.out.println("Selected Features by Group 'ID=jg10174.t1': " + selectedFeatures);
+// FeatureList selectedFeatures = gff3.selectByGroup("ID=jg10174.t1");
+// System.out.println("Selected Features by Group 'ID=jg10174.t1': " +
+// selectedFeatures);
 
-        } catch (
+// } catch (
 
-        Exception e) {
-            e.printStackTrace();
-        }
+// Exception e) {
+// e.printStackTrace();
+// }
 
-    }
+// }
 
-    public static void loopGFF() {
-        // get filename
-        String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
-        // create File object
-        File file = new File(filepath);
-        // get GffParser for GFF3
-        GffParser parser = GffParser.getParser(3);
-        // parse the file
-        try {
-            FeatureList gff3 = parser.parse(file);
+// public static void loopGFF() {
+// // get filename
+// String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
+// // create File object
+// File file = new File(filepath);
+// // get GffParser for GFF3
+// GffParser parser = GffParser.getParser(3);
+// // parse the file
+// try {
+// FeatureList gff3 = parser.parse(file);
 
-            int count = 0;
-            for (FeatureI feature : gff3) {
+// int count = 0;
+// for (FeatureI feature : gff3) {
 
-                String seqname = feature.seqname();
-                String group = feature.group();
-                // location=[L=2056; S=-34913014; E=-34910958]
-                Location location = feature.location(); // length , start , end (-ve for reverse strand)
-                String type = feature.type();
-                HashMap<String, String> attributes = feature.getAttributes(); // also getAttribute(key)
-                System.out.println("Feature " + count + ": seqname=" + seqname + ", group=" + group
-                        + ", location=" + location + ", type=" + type + ", attributes=" + attributes);
+// String seqname = feature.seqname();
+// String group = feature.group();
+// // location=[L=2056; S=-34913014; E=-34910958]
+// Location location = feature.location(); // length , start , end (-ve for
+// reverse strand)
+// String type = feature.type();
+// HashMap<String, String> attributes = feature.getAttributes(); // also
+// getAttribute(key)
+// System.out.println("Feature " + count + ": seqname=" + seqname + ", group=" +
+// group
+// + ", location=" + location + ", type=" + type + ", attributes=" +
+// attributes);
 
-                count++;
-                if (count >= 3) {
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+// count++;
+// if (count >= 3) {
+// break;
+// }
+// }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
 
-    }
-}
+// }
+// }
--- geneviz-java/src/main/java/GVJ/testing/SWTC_004_test_GffUtils.java
+++ geneviz-java/src/main/java/GVJ/testing/SWTC_004_test_GffUtils.java
@@ -1,129 +1,134 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package GVJ.testing;
+// /*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt
+// to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit
+// this template
+// */
+// package GVJ.testing;
 
-import GVJ.io.GffParser;
+// import GVJ.io.GffParser;
 
-import static GVJ.utils.GffUtils.countFeaturesAccrossAllGenes;
-import static GVJ.utils.GffUtils.countFeaturesInGene;
-import static GVJ.utils.GffUtils.getAllGeneIds;
+// import static GVJ.utils.GffUtils.countFeaturesAccrossAllGenes;
+// import static GVJ.utils.GffUtils.countFeaturesInGene;
+// import static GVJ.utils.GffUtils.getAllGeneIds;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
+// import java.io.File;
+// import java.io.IOException;
+// import java.util.List;
 
-import org.biojava.nbio.genome.parsers.gff.FeatureList;
+// import org.biojava.nbio.genome.parsers.gff.FeatureList;
 
-/**
- *
- * @author mspriggs
- */
-public class SWTC_004_test_GffUtils {
-    public static void main(String[] args) {
-        System.out.println("SWTC_004_test_GffStatistics");
+// /**
+// *
+// * @author mspriggs
+// */
+// public class SWTC_004_test_GffUtils {
+// public static void main(String[] args) {
+// System.out.println("SWTC_004_test_GffStatistics");
 
-        // getAverageExonsPerGene
-        // try {
-        // test_getAverageExonsPerGene();
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // }
-        // longestShortestFeatures
-        // test_longestShortestFeatures();
-        // averageGeneLength
-        // test_averageGeneLength();
-        // getAllGeneIds
-        test_getAllGeneIds();
-        // test test_countFeaturesInGene
-        // test_countFeaturesInGene();
-    }
+// // getAverageExonsPerGene
+// // try {
+// // test_getAverageExonsPerGene();
+// // } catch (IOException e) {
+// // e.printStackTrace();
+// // }
+// // longestShortestFeatures
+// // test_longestShortestFeatures();
+// // averageGeneLength
+// // test_averageGeneLength();
+// // getAllGeneIds
+// test_getAllGeneIds();
+// // test test_countFeaturesInGene
+// // test_countFeaturesInGene();
+// }
 
-    private static void test_getAverageExonsPerGene() throws IOException {
+// private static void test_getAverageExonsPerGene() throws IOException {
 
-        // get filename
-        String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
-        // create File object
-        File file = new File(filepath);
-        // get GffParser for GFF3
-        GffParser parser = GffParser.getParser(3);
-        // parse the file
-        try {
-            FeatureList gff3 = parser.parse(file);
+// // get filename
+// String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
+// // create File object
+// File file = new File(filepath);
+// // get GffParser for GFF3
+// GffParser parser = GffParser.getParser(3);
+// // parse the file
+// try {
+// FeatureList gff3 = parser.parse(file);
 
-            double avgExons = countFeaturesAccrossAllGenes(gff3, "exons");
-            System.out.println("Average exons per gene: " + avgExons);
+// double avgExons = countFeaturesAccrossAllGenes(gff3, "exons");
+// System.out.println("Average exons per gene: " + avgExons);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+// }
 
-    private static void test_longestShortestFeatures() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from
-                                                                       // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+// private static void test_longestShortestFeatures() {
+// throw new UnsupportedOperationException("Not supported yet."); // Generated
+// from
+// // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+// }
 
-    private static void test_averageGeneLength() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from
-                                                                       // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+// private static void test_averageGeneLength() {
+// throw new UnsupportedOperationException("Not supported yet."); // Generated
+// from
+// // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+// }
 
-    private static void test_getAllGeneIds() {
-        // get filename
-        String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
-        // String filepath = "geneviz-java/src/main/java/GVJ/data/test.gtf";
+// private static void test_getAllGeneIds() {
+// // get filename
+// String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
+// // String filepath = "geneviz-java/src/main/java/GVJ/data/test.gtf";
 
-        // create File object
-        File file = new File(filepath);
-        // get GffParser for GFF3
-        GffParser parser = GffParser.getParser(3);
-        // GffParser parser = GffParser.getParser(2);
+// // create File object
+// File file = new File(filepath);
+// // get GffParser for GFF3
+// GffParser parser = GffParser.getParser(3);
+// // GffParser parser = GffParser.getParser(2);
 
-        // parse the file
-        try {
-            FeatureList gff3 = parser.parse(file);
+// // parse the file
+// try {
+// FeatureList gff3 = parser.parse(file);
 
-            List<String> geneIds = getAllGeneIds(gff3);
-            for (String id : geneIds) {
-                System.out.println("Gene ID: " + id);
-            }
+// List<String> geneIds = getAllGeneIds(gff3);
+// for (String id : geneIds) {
+// System.out.println("Gene ID: " + id);
+// }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
 
-    }
+// }
 
-    private static void test_countFeaturesInGene() {
-        // get filename
-        // String filepath = "geneviz-java/src/main/java/GVJ/data/test.gff";
-        // String filepath = "geneviz-java/src/main/java/GVJ/data/test.gtf";
-        String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
-        // String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gtf";
+// private static void test_countFeaturesInGene() {
+// // get filename
+// // String filepath = "geneviz-java/src/main/java/GVJ/data/test.gff";
+// // String filepath = "geneviz-java/src/main/java/GVJ/data/test.gtf";
+// String filepath = "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gff";
+// // String filepath =
+// "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.gtf";
 
-        // create File object
-        File file = new File(filepath);
-        // get GffParser for GFF3
-        GffParser parser = GffParser.getParser(3);
-        // parse the file
-        try {
-            FeatureList gff3 = parser.parse(file);
+// // create File object
+// File file = new File(filepath);
+// // get GffParser for GFF3
+// GffParser parser = GffParser.getParser(3);
+// // parse the file
+// try {
+// FeatureList gff3 = parser.parse(file);
 
-            System.out.println("Counting exons in All genes:");
-            double avgExons = countFeaturesAccrossAllGenes(gff3, "exon");
-            System.out.println("Average exons per gene: " + avgExons);
+// System.out.println("Counting exons in All genes:");
+// double avgExons = countFeaturesAccrossAllGenes(gff3, "exon");
+// System.out.println("Average exons per gene: " + avgExons);
 
-            String testGeneId = "jg10174";
-            System.out.println("Counting exons in gene: " + testGeneId);
-            double exonCountInGene = countFeaturesInGene(gff3, "exon", testGeneId);
-            System.out.println("Exon count in " + testGeneId + ": " + exonCountInGene);
+// String testGeneId = "jg10174";
+// System.out.println("Counting exons in gene: " + testGeneId);
+// double exonCountInGene = countFeaturesInGene(gff3, "exon", testGeneId);
+// System.out.println("Exon count in " + testGeneId + ": " + exonCountInGene);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
 
-    }
+// }
 
-}
+// }
