407dae514d1c2b389feb827862654f780a4f5f6e
Matthew Spriggs <matspriggs@gmail.com>
2025-11-24 17:21:02 +0000

    Add parsers and utility classes for handling FASTA and GFF files
    - Implemented FastaParser for parsing FASTA files into DNASequence maps.
    - Created GffParser as an abstract class with Gff2Parser and Gff3Parser implementations for parsing GFF files.
    - Developed DataManager to manage parsed FASTA and GFF data.
    - Added utility classes FastaUtils and GffUtils for statistical analysis and feature extraction from FASTA and GFF data.
    - Created test classes for FastaParser, GffParser, and utility functions to validate functionality.

---
+++ geneviz-java/src/main/java/GVJ/testing/SWTC_005_test_FastaStatistics.java
@@ -0,0 +1,81 @@
+// /*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt
+// to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit
+// this template
+// */
+// package GVJ.testing;
+
+// import GVJ.io.FastaParser;
+
+// import static GVJ.utils.FastaUtils.getAverageGCpct;
+// import static GVJ.utils.FastaUtils.getSequenceLength;
+// import static GVJ.utils.FastaUtils.sequencesGetAverageLength;
+
+// import java.io.File;
+// import java.util.Map;
+// import org.biojava.nbio.core.sequence.DNASequence;
+
+// /**
+// *
+// * @author mspriggs
+// */
+// public class SWTC_005_test_FastaStatistics {
+
+// public static void main(String[] args) {
+// System.out.println("SWTC_005_test_FastaStatistics");
+
+// try {
+// // crteate local file
+// String filepath =
+// "geneviz-java/src/main/java/GVJ/data/RiAB_ragtag_HiC.fasta";
+// File file = new File(filepath);
+
+// // get DNAMap from FastaParser for testing.
+// Map<String, DNASequence> fastaMap = FastaParser.parseFasta(file);
+
+// // get individual FASTA sequence
+// DNASequence fastaSequence = fastaMap.get("HiC_scaffold_1_RagTag");
+
+// // test getLength
+// test_getSequenceLength(fastaSequence);
+
+// // test SequencesGetAverageLength
+// test_sequencesGetAverageLength(fastaMap);
+
+// // test getSequenceGCcount
+// test_getSequenceGCcount(fastaMap);
+
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+
+// }
+
+// public static void test_getSequenceLength(DNASequence fastaSequence) {
+// System.out.println("SWTC_005_test_FastaStatistics.test_getLength");
+
+// int length = getSequenceLength(fastaSequence);
+// System.out.println("FASTA Length: " + length);
+
+// }
+
+// public static void test_sequencesGetAverageLength(Map<String, DNASequence>
+// fastaMap) {
+// System.out.println("SWTC_005_test_FastaStatistics.test_sequencesGetAverageLength");
+// double avgLength = sequencesGetAverageLength(fastaMap);
+// System.out.println("FASTA Average Length in base-pairs: " + avgLength);
+// double avgLengthMbp = sequencesGetAverageLength(fastaMap, true);
+// System.out.println("FASTA Average Length in Mbp: " + avgLengthMbp);
+
+// }
+
+// public static void test_getSequenceGCcount(Map<String, DNASequence> fastaMap)
+// {
+// System.out.println("SWTC_005_test_FastaStatistics.test_getSequenceGCcount");
+
+// double gcContent = getAverageGCpct(fastaMap);
+// System.out.println("FASTA GC Content (%): " + gcContent);
+
+// }
+// }
